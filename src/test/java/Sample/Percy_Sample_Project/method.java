9df67778b6b31533b53ca2b6f53eced58f4abe1a
Manoj Sirsat <manoj.sirsat@galaxyweblinks.in>
2022-12-06 20:05:44 +0530

    Java code

--- src/test/java/Sample/Percy_Sample_Project/method.java
+++ src/test/java/Sample/Percy_Sample_Project/method.java
@@ -2,15 +2,22 @@ package Sample.Percy_Sample_Project;
 
 import java.util.Arrays;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.Test;
 public class method extends Base_Class {
 
 
     @Test
-    public void snapshotCompare() {
+    public void snapshotCompare() throws InterruptedException {
       
         driver.get("https://dev-wev.pantheonsite.io/explore-entrepreneurship/");
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//a[@id='wt-cli-accept-all-btn']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//span[@class='emergency-close']")).click();
+        Thread.sleep(2000);
+        
        JavascriptExecutor js = (JavascriptExecutor) driver;
        js.executeScript("var element = document.getElementsByTagName('BODY')[0];"+"var parent = element.parentNode;"+"var wrapper = document.createElement('div');"+"wrapper.className='canvasWrapper';"+"parent.replaceChild(wrapper, element);"+"wrapper.appendChild(element);");
 
